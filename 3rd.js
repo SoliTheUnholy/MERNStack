58e89d3ceb34dd25176c047fa8b7355cb217a082
SoliTheUnholy <soheilzss82@gmail.com>
2023-07-25 20:02:53 +0330

    tamri sevom ezafe shod

---
+++ 3rd.js
@@ -0,0 +1,10 @@
+function amin(number) {
+    switch (number) {
+        case 10:
+            return "adad 10 hasr"
+        case 15:
+            return "adad 15 hast"
+    }
+    return "Error"
+}
+console.log(amin(+prompt("adad rsa vared konid")))
